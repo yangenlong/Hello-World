a2900f81938373181b925510e4e5a93783178443
yangenlong <52478439+yangenlong@users.noreply.github.com>
2019-08-14 09:08:08 +0800

    Add files via upload

--- led.h
+++ led.h
@@ -2,10 +2,11 @@
 #define __LED_H   
 #include "sys.h" 
 //LED 端口定义 
-#define LED0 PAout(8) 
-// PA8 
-#define LED1 PDout(2)
-// PD2   
+#define LED0 PAout(5) 
+// PA5
+#define LED1 PEout(5)
+// PE5   
 void LED_Init(void);
-//初始化            
+//初始化  
+
 #endif 
