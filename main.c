f7ddeb464a7f6cbb9ae0d1928cc98003de0b439e
yangenlong <52478439+yangenlong@users.noreply.github.com>
2019-08-15 13:40:39 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,20 +1,37 @@
-#include "sys.h"
-#include "delay.h"
-#include "usart.h"
 #include "led.h"
+#include "delay.h"
+#include "sys.h"
+#include "key.h"
 
- int main(void)
- {	
-	delay_init();	    //延时函数初始化	  
-	LED_Init();		  	//初始化与LED连接的硬件接口
+
+	int main(void)
+{
+		u8 t;//unsigned char
+	delay_init(); //延时函数初始化
+	LED_Init(); //初始化与 LED 连接的硬件接口
+	KEY_Init(); //初始化与按键连接的硬件接口
+	//	BRRP_Init();蜂鸣器
+	LED0=0; //点亮 LED
 	while(1)
 	{
-		LED0=0;
-		LED1=1;
-		delay_ms(300);	
-		//延时300ms
-		LED0=1;
-		LED1=0;
-		delay_ms(300);	
-		//延时300ms
-	}}
+		t=KEY_Scan(0); //得到键值
+		switch(t)
+		{
+			case KEY0_PRES:
+				LED0=!LED0;
+				break;
+			case KEY1_PRES:
+				LED1=!LED1;
+				break;
+			case WKUP_PRES:
+				LED0=!LED0;
+				LED1=!LED1;
+				break;
+	default:
+	delay_ms(10);
+		}	
+	}
+}
+
+
+
